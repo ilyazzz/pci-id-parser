e835b2d5e2220690f3e1715a739733c5c66b7b92
Ilya Zlobintsev <ilya.zl@protonmail.com>
2021-05-31 07:27:12 +0300

    Remove everything but scripts

--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 pci-parser &&
 patch < patch.diff &&
-#echo $(date -u "+%y/%m/%d %H:%M") > date
-echo $(date -u "+%s") > date &&
-cp devices.json /srv/http/ &&
-cp date /srv/http 
+
+mv devices.json out &&
+mv index.html out
